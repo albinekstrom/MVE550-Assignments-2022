80c59e7508c6ea43aabd8bb900b842dfe3d2ceb2
Akmp40 <Akmp40@gmail.com>
2022-11-17 10:50:57 +0100

    Cont. part 2

--- First Assignment/assignment1-2.r
+++ First Assignment/assignment1-2.r
@@ -6,14 +6,18 @@ mdata<-c(0,p,p,p,p,0,
          1,0,0,0,0,0,
          0,0,0,0,0,1)
 m<-matrix(mdata,6,6,byrow = TRUE)
+a<-c(p,p,p,p,0,0)
 
 #a
 Q<-m[0:5,0:5]
 R<-m[0:5,6]
 O<-m[6,0:5]
 I<-diag(5)
-F=solve(I-Q)#^(-1)
-sum(F[1,])
+F=solve(I-Q) # Solve gives inverse 
+#sum(F[1,])
+
+b<-F%*%c(1,1,1,1,1)
+tot_throws = b[1]*p+b[2]*p+b[3]*p+b[4]*b[5]*0+1
 
 #b
 N=solve(I-Q)^(-1)
