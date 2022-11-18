854391af88edca743dc9168887720cc0156f3298
albinekstrom <albineks20@gmail.com>
2022-11-18 15:18:42 +0100

    Final submission update

--- First Assignment/Assignment 2.R
+++ First Assignment/Assignment 2.R
@@ -16,28 +16,27 @@ O<-m[5,0:4]
 I<-diag(4)
 F=solve(I-Q)
 NOF <- F%*%c(1,1,1,1) # Number of throws
-1/4*(NOF[1] + NOF[2] + NOF[3] + NOF[4]) + 1 # Total throws
+throws <- 1/4*(NOF[1] + NOF[2] + NOF[3] + NOF[4]) + 1 # Total throws
+cat("Expected number of throws: ", throws)
 
 
-## b) Probability that the player will visit state 3
-p<-1/4
-        #1,2,4,3,6
-mdata<-c(p,p,p,p,0,   #1
-         p,0,p,p,p,   #2
-         2*p,p,0,0,p, #4
-         0,0,0,1,0,   #3
-         0,0,0,0,1)   #6
-m<-matrix(mdata,5,5,byrow = TRUE)
+## b) Probability that the player will visit state 3 
 
-Q<-m[0:3,0:3]
-R<-m[0:3,4:5]
-O<-m[4:5,0:3]
+P<-matrix(c(F[1,1],0,0,0,
+            0, F[2,2],0,0,
+            0,0,F[3,3],0,
+            0,0,0,F[4,4]),4,4)
+H <- (F-diag(4))%*%solve(P)
+prob <- sum(1/4*H[c(1,2,4),3])+1/4 
+cat("Probability of visiting state 3: ", prob)
 
-I <- diag(3)
-F <- solve(I-Q)
-FR <- F%*%R # Prob for stepping on 3 or 6 from 1,2 or 4
 
-1/4*sum(FR[1:3,1])+1/4
+## c) Probability that the player will visit state 3 two or more
+
+p_0 <- (1/4*(1-H[1,3])+1/4*(1-H[2,3])+1/4*(1-H[4,3])) # Chance to never visit 3
+p_1 <- (1/4)+((1/4*H[1,3])+1/4*(H[2,3])+1/4*(H[4,3]))*(1-H[3,3]) # Chance to visit 3 once
+prob_2_more <- 1-(p_0+p_1)
+cat("Probability of visiting state 3 two or more times: ", prob_2_more)
 
 
 
