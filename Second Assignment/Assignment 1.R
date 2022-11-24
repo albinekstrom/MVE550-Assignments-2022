d7113f24f5307bfbf3d1a9d66ebc140616d7164b
Akmp40 <Akmp40@gmail.com>
2022-11-24 11:00:43 +0100

    Second assignment update

--- Second Assignment/Assignment 1.R
+++ Second Assignment/Assignment 1.R
@@ -11,10 +11,18 @@ P2 = c(1, 0, 2)
 P3 = c(1, 2, 0)
 
 # E(Pi | data)
-EP1 = (c(1, 1, 1) + P1) / (length(P1) + P1)
-EP2 = (c(1, 1, 1) + P2) / (length(P2) + P2)
-EP3 = (c(1, 1, 1) + P3) / (length(P3) + P3)
+EP1 = (c(1, 1, 1) + P1) / (length(P1) + sum(P1))
+EP2 = (c(1, 1, 1) + P2) / (length(P2) + sum(P2))
+EP3 = (c(1, 1, 1) + P3) / (length(P3) + sum(P3))
   
 # Expectation of the posterior for P
-matrix(c(EP1, EP2, EP3), 3, 3, byrow = TRUE)
+m = matrix(c(EP1, EP2, EP3), 3, 3, byrow = TRUE)
 
+y=c(1,2,3,2,3,1,2,1,3,2)
+
+for (i in (length(y)+1):20)
+{
+  t = tail(y,1)
+  y[i]=sample(c(1,2,3),1,prob = m(t,))
+  
+}
