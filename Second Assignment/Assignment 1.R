54534aa82cfe58f811d989a0950e189680f6ec43
Akmp40 <Akmp40@gmail.com>
2022-11-28 15:10:10 +0100

    Assignment 2 progress

--- Second Assignment/Assignment 1.R
+++ Second Assignment/Assignment 1.R
@@ -6,8 +6,10 @@ library(LearnBayes)
 # Pi ∼ Dirichlet(1,1,1)
 
 # Functions
+
 get_tmatrix <- function(y)
 {
+  # Get counts of transitions in transition matrix
   m = matrix(0, nrow = 3,ncol = 3,byrow = T)
   for (i in 1:length(y)-1)
   {
@@ -25,7 +27,8 @@ get_expected_m <- function(y)
   P2 = m[2,]
   P3 = m[3,]
   
-  # E(Pi | data)
+  
+  # E(Pi | data) Pg 28. in comp.
   EP1 = (c(1, 1, 1) + P1) / (length(P1) + sum(P1))
   EP2 = (c(1, 1, 1) + P2) / (length(P2) + sum(P2))
   EP3 = (c(1, 1, 1) + P3) / (length(P3) + sum(P3))
@@ -33,6 +36,7 @@ get_expected_m <- function(y)
   # Expectation of the posterior for P
   return(matrix(c(EP1, EP2, EP3), 3, 3, byrow = TRUE))
 }
+
 
 simulate1 <- function(y){
   m=get_expected_m(y)
@@ -90,9 +94,9 @@ simulate4 <- function(y){
   m = m1
   for (i in (length(y)+1):400)
   {
-    t = tail(y,1)
-    Psamp = sample(1,)
-    y[i]=sample(c(1,2,3),1,prob = m1[t,])
+    t = tail(y,1) # last seen value
+    Psamp = rdirichlet(1,m1[t,])
+    y[i]=sample(c(1,2,3),1,prob = Psamp)
     m=get_expected_m(y)
   }
   return (m[1,2])
@@ -112,19 +116,20 @@ P3 = P[3,]
 
 # 1.b
 
-simulate1(y)
-hist(y)
+hist(simulate1(y))
+
 
 # 1.c
-expected_p12 = replicate(1000,simulate2(y))
-hist(expected_p12)
+expected_p12_c = replicate(1000,simulate2(y))
+hist(expected_p12_c,xlim = c(0,1))
 
 # 1.d
-expected_p12 = replicate(1000,simulate3(y))
-hist(expected_p12)
+expected_p12_d = replicate(1000,simulate3(y))
+hist(expected_p12_d,xlim = c(0,1))
 
 # 1.e
-
+expected_p12_e = replicate(1000,simulate4(y))
+hist(expected_p12_e,xlim = c(0,1))
 
 #m_count = matrix(c(length(which(y == 1)),
 #         length(which(y == 2)),
