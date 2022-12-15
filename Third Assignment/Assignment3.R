6c284c40a397df0957e44a6a421e37a0b34d4ab9
Akmp40 <Akmp40@gmail.com>
2022-12-15 10:39:54 +0100

    Part 1 update

--- Third Assignment/Assignment3.R
+++ Third Assignment/Assignment3.R
@@ -4,25 +4,44 @@ y = unlist(data["y"],use.names = FALSE)
 z = unlist(data["z"],use.names = FALSE)
 
 
-sick<-which(z==1)
+healthy<-which(-(z-1) ==1)
 plot(x,y)
-points(x[sick],y[sick],col='red')
+#points(x[sick],y[sick],col='red')
 
-f_d <- function(x,y,theta_1,theta_2,theta_3){
-  a=exp(exp(theta_1)*x + exp(theta_2)*((y-theta_3)^2))
-  return((a-1)/(a+1))
+f_d <- function(x_d,y_d,theta_1,theta_2,theta_3){
+  a <- exp(exp(theta_1)*x_d + exp(theta_2)*((y_d-theta_3)^2))
+  #if((a-1)/(a+1)>1){}
+  return ((a-1)/(a+1))
+
 }
 
 Likelihood <- function(theta){
-  return(prod(f_d(x,y,theta[1],theta[2],theta[3])))
+  
+  lh = f_d(x,y,theta[1],theta[2],theta[3])
+  lh[healthy] = 1-lh[healthy]
+  return(prod(lh))
 }
+Likelihood_test <- function(theta){
+  lh = f_d(x,y,theta[1],theta[2],theta[3])
+  lh[healthy] = 1-lh[healthy]
+  return(lh)
+}
+Likelihood_test_2 <- function(theta){
+  lh = f_d(x,y,theta[1],theta[2],theta[3])
+  return(lh)
+  
+}
+
 
 
 post <- function(theta){
   return(log(Likelihood(theta)))
 }
 
-
+log_a <- function(theta){
+  loga <- log(Likelihood(theta))
+  return(loga)
+}
 
 
 
@@ -36,24 +55,37 @@ post(c(1,1,20))
 N <- 10000
 result <- matrix(0,N,3)
 #Initialize some starting theta: 
-result[1,] <- c(1, 1, 15)
+result[1,] <- c(-4, -2, 16)
+#result[1,] <- c(-1.281, -21.559, 215.430)
+print(c("Start LH", Likelihood(result[1,])))
+prop=c()
 for (i in 2:N) {
-  prop <- rnorm(3,result[i-1,],0.4)
-  while(is.nan(post(prop))){
-    prop <- rnorm(3,result[i-1,],0.4)
-  }
-  print(prop)
-  print(post(prop))
-  
-  if (runif(1)<post(prop)/post(result[i-1,]))
+  for(j in 1:3){prop[j] <- result[i-1,j] + rnorm(1,0,0.4)}
+  a <-  post(prop) - post(result[i-1,])
+  if (runif(1)<exp(a)){
     result[i,] <- prop
-  else
+    #print(c("accepted",exp(a)))
+  }
+  else{
     result[i,] <- result[i-1,]
+    #print(c("declined",exp(a)))
+  }
+  
+  #print(c("Likelyhood",Likelihood(result[i,])))
+  
 }
+
+print(c("End LH", Likelihood(result[N,])))
+print(c("Result", result[N,]))
+
 plot(result[,1],type='l')
-lines(result[,2],col='red')
-lines(result[,3],col='blue')
+#plot(result[,2],type='l',col='red')
+#plot(result[,3],type='l',col='blue')
 
+f_d(x,y,result[N,1],result[N,2],result[N,3])
 
-
-
+#d
+t<- c(-1.70514738051966,-3.47251580511398, 17.2761895227376)
+#f_d(x,y,result[N,1],result[N,2],result[N,3])
+ch=f_d(3,13,t[1],t[2],t[3])
+dbinom(9,10,ch)
